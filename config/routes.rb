bd976c8e1179506aead8ef813432d53278dd78e2
nbjun1 <138277763+nbjun1@users.noreply.github.com>
2023-09-06 16:03:04 +0000

    0906

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  root "public/homes#top"
+  namespace :public do
+    get 'homes/top'
+    get 'homes/about'
+  end
+  devise_for :admins
+  devise_for :members
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
