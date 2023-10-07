f68ecf135ccdc01ca5b76fc4b953a713fe4c1608
nbjun1 <x42revolution@gmail.com>
2023-10-07 11:26:43 +0000

    fix result

--- db/schema.rb
+++ db/schema.rb
@@ -111,8 +111,8 @@ ActiveRecord::Schema.define(version: 2023_10_04_092904) do
     t.datetime "updated_at", precision: 6, null: false
     t.integer "member_id"
     t.integer "genre_id"
-    t.integer "prefecture_id"
-    t.integer "city_id"
+    t.string "prefecture"
+    t.string "city"
     t.string "title", default: "", null: false
     t.text "body"
     t.string "name", default: "", null: false
