a0ad067e2953028d5e1465eee8205e3c22b37786
nbjun1 <x42revolution@gmail.com>
2023-10-15 07:17:32 +0000

    fix comment

--- app/controllers/member/comments_controller.rb
+++ app/controllers/member/comments_controller.rb
@@ -5,7 +5,6 @@ class Member::CommentsController < ApplicationController
     @comment = current_member.comments.new(comment_params)
     @comment.result_id = @result.id
     @comment.save
-    redirect_to member_result_show_path(@result)
   end
 
   def update
@@ -15,7 +14,6 @@ class Member::CommentsController < ApplicationController
     @comment = Comment.find(params[:id])
     @result = @comment.result
     @comment.destroy
-    redirect_to member_result_show_path(@result), notice: "コメントを削除しました"
   end
 
   private
